53fbae6aa5a76b40f31b60a096ec1c9dcc235b77
llcoll5 <collmaslluis@gmail.com>
2026-01-30 12:32:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,23 +25,25 @@ if __name__ == "__main__":
             print(f"Game ID: {game_id}, Rating: {rating}")
             print(f"URL: {games_urls.get(game_id, 'URL not found')}")
 
-    html = """
-    <html>
-        <body>
-            <h1>Partits recomanats</h1>
-            <ul>
-                {list_items}
-            </ul>
-        </body>
-    </html>
-    """
+    if len(worth_watching) > 0:
+        html = """
+        <html>
+            <body>
+                <h2>De tots els partits jugats aquesta matinada, destaquen els següents:</h2>
+                <ul>
+                    {list_items}
+                </ul>
+            </body>
+        </html>
+        """
 
-    list_items = ""
-    for game_id in worth_watching:
-        list_items += f"<li><a href=\"{games_urls.get(game_id, '#')}\">Partit {' VS '.join(nba.get_teams_from_game_code(game_id))}</a></li>"
+        list_items = ""
+        for game_id in worth_watching:
+            list_items += f"<li><a href=\"{games_urls.get(game_id, '#')}\">Partit {' VS '.join(nba.get_teams_from_game_code(game_id))}</a></li>"
 
-    html = html.format(list_items=list_items)
+        html = html.format(list_items=list_items)
+        subject = f"{len(worth_watching)} Bons partits NBA d'aquesta matinada - {time.strftime('%d/%m/%Y')}"
 
-    sender.send_email(f"Bons partits NBA d'aquesta matinada - {time.strftime('%d/%m/%Y')}", html, receiver)
+        sender.send_email(subject, html, receiver)
 
-    print(f"HTML enviat: {html}")
+        print(f"HTML enviat: {html}")
