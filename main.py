c6977c53a2406fc25809b0a3fda9483e1004dd71
llcoll5 <collmaslluis@gmail.com>
2026-01-31 17:26:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,33 @@ from NBA import NBA
 from Wikihoops import Wikihoops
 from Sender import Sender
 
-if __name__ == "__main__":
-    load_dotenv()
-    username = os.getenv("EMAIL_SENDER")
-    password = os.getenv("EMAIL_PASSWORD")
-    receiver = os.getenv("EMAIL_RECEIVERS")
+def receiver_list(receivers_str):
+    return [email.strip() for email in receivers_str.split(",")]
 
+def send_email_games(sender, games, games_urls, receiver, nba):
+    subject = f"{len(games)} Bons partits NBA d'aquesta matinada - {time.strftime('%d/%m/%Y')}"
+    html = """
+    <html>
+        <body>
+            <h2>De tots els partits jugats aquesta matinada, destaquen els següents:</h2>
+            <ul>
+                {list_items}
+            </ul>
+        </body>
+    </html>
+    """
+
+    list_items = ""
+    for game_id in games:
+        list_items += f"<li><a href=\"{games_urls.get(game_id, '#')}\">Partit {' VS '.join(nba.get_teams_from_game_code(game_id))}</a></li>"
+
+    html = html.format(list_items=list_items)
+    try:
+        sender.send_email(subject, html, receiver)
+    except Exception as e:
+        print(f"Error enviant l'email a {receiver}: {e}")
+
+def main():
     sender = Sender(username, password)
     nba = NBA()
     wikihoops = Wikihoops()
@@ -26,24 +47,13 @@ if __name__ == "__main__":
             print(f"URL: {games_urls.get(game_id, 'URL not found')}")
 
     if len(worth_watching) > 0:
-        html = """
-        <html>
-            <body>
-                <h2>De tots els partits jugats aquesta matinada, destaquen els següents:</h2>
-                <ul>
-                    {list_items}
-                </ul>
-            </body>
-        </html>
-        """
+        for receiver in receiver_list(receivers):
+            send_email_games(sender, worth_watching, games_urls, receiver, nba)
 
-        list_items = ""
-        for game_id in worth_watching:
-            list_items += f"<li><a href=\"{games_urls.get(game_id, '#')}\">Partit {' VS '.join(nba.get_teams_from_game_code(game_id))}</a></li>"
+if __name__ == "__main__":
+    load_dotenv()
+    username = os.getenv("EMAIL_SENDER")
+    password = os.getenv("EMAIL_PASSWORD")
+    receivers = os.getenv("EMAIL_RECEIVERS")
 
-        html = html.format(list_items=list_items)
-        subject = f"{len(worth_watching)} Bons partits NBA d'aquesta matinada - {time.strftime('%d/%m/%Y')}"
-
-        sender.send_email(subject, html, receiver)
-
-        print(f"HTML enviat: {html}")
+    main()
